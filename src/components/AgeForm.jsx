8c87abdc9925f625777ec56eb074d4f125053d9f
Luiza Moraes <luizamferre@gmail.com>
2024-06-06 17:48:37 -0300

    Update CSS and JSX files, remove react.svg, and add icon-arrow.svg

--- src/components/AgeForm.jsx
+++ src/components/AgeForm.jsx
@@ -1,24 +1,41 @@
 // src/components/AgeForm.js
 
 import React from "react";
+import SubmitBtn from "../assets/icon-arrow.svg";
 
 const AgeForm = ({ onSubmit }) => {
   return (
-    <form onSubmit={(e) => e.preventDefault()}>
-      <div>
-        <label>Day</label>
-        <input type="number" />
+    <div className="card">
+      <div className="innercard1">
+      <div className="container">
+        <form onSubmit={(e) => e.preventDefault()}>
+          <div className="form_container">
+            <div className="block">
+              <label htmlFor="day">Day</label>
+              <input type="number" id="day" placeholder="DD" />
+              <small></small>
+            </div>
+            <div className="block">
+              <label htmlFor="month">Month</label>
+              <input type="number" id="month" placeholder="MM" />
+              <small></small>
+            </div>
+            <div className="block">
+              <label htmlFor="year">Year</label>
+              <input type="number" id="year" placeholder="YYYY" />
+              <small></small>
+            </div>
+          </div>
+          <div className="submit_block">
+            <hr />
+            <button type="submit" className="submit_btn">
+              <img src={SubmitBtn}></img>
+            </button>
+          </div>
+        </form>
       </div>
-      <div>
-        <label>Month</label>
-        <input type="number" />
       </div>
-      <div>
-        <label>Year</label>
-        <input type="number" />
-      </div>
-      <button type="submit">Calculate Age</button>
-    </form>
+    </div>
   );
 };
 
--- src/components/AgeResult.jsx
+++ src/components/AgeResult.jsx
@@ -4,10 +4,16 @@ import React from "react";
 
 const AgeResult = ({ age }) => {
   return (
-    <div>
-      <p>years</p>
-      <p>months</p>
-      <p>days</p>
+    <div className="card">
+      <div className="innercard2">
+      <div className="container">
+        <div className="output">
+          <h1><span id="YY">--</span>years</h1>
+          <h1><span id="MM">--</span>months</h1>
+          <h1><span id="DD">--</span>days</h1>
+        </div>
+      </div>
+      </div>
     </div>
   );
 };
